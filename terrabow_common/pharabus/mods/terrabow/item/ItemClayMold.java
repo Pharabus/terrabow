284e4c0aee5dc1fe18d887c7f9ec92236dd5e8c6
Andrew Meehan <andrew@meehan.me.uk>
2013-09-30 06:34:28 -0400

    Claymolds update

--- terrabow_common/pharabus/mods/terrabow/item/ItemClayMold.java
+++ terrabow_common/pharabus/mods/terrabow/item/ItemClayMold.java
@@ -1,5 +1,10 @@
 package pharabus.mods.terrabow.item;
 
+import pharabus.mods.terrabow.TerraBowSettings;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.Icon;
 import TFC.Core.TFCTabs;
 import TFC.Items.Pottery.ItemPotteryMold;
@@ -15,6 +20,33 @@ public class ItemClayMold extends ItemPotteryMold {
     public ItemClayMold(int id) {
         super(id);
         setCreativeTab(TFCTabs.TFCPottery);
+       
+    }
+    
+    @Override
+    public Icon getIconFromDamage(int damage) {
+        if (damage == 0) return this.ClayIcon;
+        if (damage == 1) return this.CeramicIcon;
+        if (damage == 2) return this.CopperIcon;
+        if (damage == 3) return this.BronzeIcon;
+        if (damage == 4) return this.BismuthBronzeIcon;
+        if (damage == 5) return this.BlackBronzeIcon;
+        
+        return this.ClayIcon;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IconRegister iconRegister) {
+               
+        this.ClayIcon = iconRegister.registerIcon(TerraBowSettings.TEXTURE_LOCATION + ":" + "pottery/" + this.MetaNames[0]);
+        this.CeramicIcon = iconRegister.registerIcon(TerraBowSettings.TEXTURE_LOCATION + ":" + "pottery/" + this.MetaNames[1]);
+        if (this.MetaNames.length > 2) {
+          this.CopperIcon = iconRegister.registerIcon(TerraBowSettings.TEXTURE_LOCATION + ":" + "pottery/" + this.MetaNames[2]);
+          this.BronzeIcon = iconRegister.registerIcon(TerraBowSettings.TEXTURE_LOCATION + ":" + "pottery/" + this.MetaNames[3]);
+          this.BismuthBronzeIcon = iconRegister.registerIcon(TerraBowSettings.TEXTURE_LOCATION + ":" + "pottery/" + this.MetaNames[4]);
+          this.BlackBronzeIcon = iconRegister.registerIcon(TerraBowSettings.TEXTURE_LOCATION + ":" + "pottery/" + this.MetaNames[5]);
+        }
     }
 
 }
